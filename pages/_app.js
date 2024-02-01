266a5abc1c44c2417d3b95e113d2dbae2d75ea17
koheis <kouheihand@yahoo.co.jp>
2024-02-01 15:08:02 +0900

    mobile2.css削除
    Co-authored-by: Coji Mizoguchi <coji@techtalk.jp>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/mobile.css'
 import '@/styles/pc.css'
-import '@/styles/mobile2.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
