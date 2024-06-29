5edb2b2de8e84b0891ffa87a5bc3ca57bef8f69f
koheis <kouheihand@yahoo.co.jp>
2024-06-29 19:30:36 +0900

    next-sitemap yarn add

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,7 @@
 
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://pianostudio-ruban.com/',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    outDir: './out',
+    siteUrl: "https://pianostudio-ruban.com",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
 };
