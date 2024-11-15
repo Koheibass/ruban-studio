09ca7f06f582ae65783057cccf67062a821347e4
koheis <kouheihand@yahoo.co.jp>
2024-11-15 20:52:10 +0900

    NEWS更新

--- components/News/Topix.jsx
+++ components/News/Topix.jsx
@@ -1,27 +1,59 @@
-
-import { useLanguage } from '@/utils/language';
-import Link from 'next/link';
+import { useLanguage } from '@/utils/language'
+import Link from 'next/link'
 
 export const Topix = () => {
-    const { lang } = useLanguage();
-    return (
-        <div className='pt-2 pb-8'>
-            <h3 className="mb-4 text-center text-[20px] lg:tracking-widest lg:text-[18px] leading-none font-bold pt-2 pb-1 text-[#937a43] font-noto mt-4">
-                <div>NEWS</div>
-            </h3>
-            <div className="grid mb-4 lg:mt-4 px-4 py-4 bg-[#f9f4ff] border rounded">
-                <div className="font-bold text-xl text-[#937a43] ml-4 pb-2">
-                    {"2024.06.22"}
-                    <small> (Sat)</small>
-                </div>
-                <Link href="/" className="block underline lg:mt-1 text-[#ff9a9a] text-xl">ピアノスタジオOPEN♪</Link>
+  const { lang } = useLanguage()
+  return (
+    <div className="pt-2 pb-8">
+      <h3 className="mb-4 text-center text-[20px] lg:tracking-widest lg:text-[18px] leading-none font-bold pt-2 pb-1 text-[#937a43] font-noto mt-4">
+        <div>NEWS</div>
+      </h3>
+      <div className="grid mb-4 lg:mt-4 px-4 py-4 bg-[#f9f4ff] border rounded">
+        <div className="font-bold text-xl text-[#937a43] ml-4 pb-2">
+          {'2024.10.29'}
+          <small> (Tue)</small>
+        </div>
+        <Link
+          href="/"
+          className="block underline lg:mt-1 text-[#ff9a9a] text-xl"
+        >
+          調律しました♪
+        </Link>
 
-                <div className="font-bold text-xl text-[#937a43] ml-4 hidden">
-                    {"2023.08.01"}
-                    <small> (Tue)</small>
-                </div>
-                <Link href="https://web.star7.jp/mypage/mobile_info.php?p=d09908f508" className="underline lg:mt-1 text-[#ff9a9a] text-xl hidden">調律しました♪</Link>
-            </div>
-        </div >
-    )
-} 
+        <div className="font-bold text-xl text-[#937a43] ml-4 hidden">
+          {'2023.08.01'}
+          <small> (Tue)</small>
+        </div>
+        <Link
+          href="https://web.star7.jp/mypage/mobile_info.php?p=d09908f508"
+          className="underline lg:mt-1 text-[#ff9a9a] text-xl hidden"
+        >
+          調律しました♪
+        </Link>
+      </div>
+      <div className="grid mb-4 lg:mt-4 px-4 py-4 bg-[#f9f4ff] border rounded">
+        <div className="font-bold text-xl text-[#937a43] ml-4 pb-2">
+          {'2024.06.22'}
+          <small> (Sat)</small>
+        </div>
+        <Link
+          href="/"
+          className="block underline lg:mt-1 text-[#ff9a9a] text-xl"
+        >
+          ピアノスタジオOPEN♪
+        </Link>
+
+        <div className="font-bold text-xl text-[#937a43] ml-4 hidden">
+          {'2023.08.01'}
+          <small> (Tue)</small>
+        </div>
+        <Link
+          href="https://web.star7.jp/mypage/mobile_info.php?p=d09908f508"
+          className="underline lg:mt-1 text-[#ff9a9a] text-xl hidden"
+        >
+          調律しました♪
+        </Link>
+      </div>
+    </div>
+  )
+}
--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,36 @@ export default function Document() {
     <Html lang="en" prefix="og: http://ogp.me/ns#">
       <Head>
         <title>【東京都】ピアノスタジオ リュバン【練馬区平和台】</title>
-        <link rel='icon' href='favicon3.ico' />
-        <link rel='apple-touch-icon' href='favicon3.ico' />
-        <meta name="description" content="【東京都】ピアノスタジオ リュバン【練馬区平和台】" />
+        <link rel="icon" href="favicon3.ico" />
+        <link rel="apple-touch-icon" href="favicon3.ico" />
+        <meta
+          name="description"
+          content="【東京都】ピアノスタジオ リュバン【練馬区平和台】"
+        />
         <meta property="og:url" content="https://pianostudio-ruban.com/" />
-        <meta property="og:image" content='https://pianostudio-ruban.com//photo/PC2.jpg' />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta
+          property="og:image"
+          content="https://pianostudio-ruban.com/photo/PC2.jpg"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="【東京都】ピアノスタジオ リュバン【練馬区平和台】"
+        />
+        <meta
+          property="og:description"
+          content="【東京都】ピアノスタジオ リュバン【練馬区平和台】"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
